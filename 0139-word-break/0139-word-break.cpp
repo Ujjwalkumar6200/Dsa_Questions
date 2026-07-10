d8c021aac427196725b6266664e268682e90b12d
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-10 12:57:08 +0530

    Time: 3 ms (72.41%), Space: 11 MB (80.59%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-
-    bool solve(int index,string & s, unordered_set<string>&st,vector<int>&dp){
-        if(index==s.length()) return true;
-
-        string word = "";
-        if(dp[index]!=-1) return dp[index];
-        for(int i =index;i<s.length();i++){
-            word+=s[i];
-            if(st.count(word)){
-                if(solve(i+1,s,st,dp))
-                return true;
-            }
-        } return dp[index]=false;
-
-    }
-
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string>st(wordDict.begin(),wordDict.end());
-        vector<int>dp(s.size(),-1);
-        return solve(0,s,st,dp);
+        vector<bool>dp(s.size(),false);
+        int n = s.length();
+        dp[n]= true;
+        for(int j =n-1;j>=0;j--){
+            string word = "";
+             for(int i =j;i<s.length();i++){
+              word+=s[i];
+                if(st.count(word)){
+                    if(dp[i+1]){
+                         dp[j]=true;
+                         true;
+                    }
+                }
+             }
+        } return dp[0];
     }
 };
