609d42beff80e12cfe1aa326f5d3794be4881c6f
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-10 13:34:10 +0530

    Time: 0 ms (100%), Space: 11.1 MB (12.23%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     vector<string> solve(int index, string &s,
-                         unordered_set<string> &st) {
+                         unordered_set<string> &st,
+                         vector<vector<string>> &dp,
+                         vector<bool> &vis) {
 
         if (index == s.size())
             return {""};
 
-        vector<string> ans;
+        if (vis[index])
+            return dp[index];
 
+        vis[index] = true;
+
+        vector<string> ans;
         string word = "";
 
         for (int end = index; end < s.size(); end++) {
@@ -16,7 +22,7 @@ public:
 
             if (st.count(word)) {
 
-                vector<string> temp = solve(end + 1, s, st);
+                vector<string> temp = solve(end + 1, s, st, dp, vis);
 
                 for (string x : temp) {
 
@@ -28,13 +34,16 @@ public:
             }
         }
 
-        return ans;
+        return dp[index] = ans;
     }
 
     vector<string> wordBreak(string s, vector<string>& wordDict) {
 
         unordered_set<string> st(wordDict.begin(), wordDict.end());
 
-        return solve(0, s, st);
+        vector<vector<string>> dp(s.size());
+        vector<bool> vis(s.size(), false);
+
+        return solve(0, s, st, dp, vis);
     }
 };
