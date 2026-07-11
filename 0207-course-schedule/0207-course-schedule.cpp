52755cbdd4329308bedc4fbfc8f9262fdb0cd179
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-11 23:45:57 +0530

    Time: 7 ms (45.38%), Space: 19.6 MB (44.98%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -16,24 +16,20 @@ public:
         for(int i =0;i<numCourses;i++){
             if(indegree[i]==0) q.push(i);
         }
-        vector<int>topo;
+        int cnt=0;
 
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            topo.push_back(node);
+            cnt++;
 
             for(auto it: adj[node]){
                 indegree[it]--;
                 if(indegree[it]==0) q.push(it);
             }
         }
-        if(topo.size()==numCourses) return true;
+        if(cnt==numCourses) return true;
         return false;
-
-
-
-
 
     }
 };
