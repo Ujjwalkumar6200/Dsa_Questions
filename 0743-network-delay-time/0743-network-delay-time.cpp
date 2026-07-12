ad89dc0901a1be96d673ba2676687379f136e4f6
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-12 18:28:53 +0530

    Time: 84 ms (54.67%), Space: 44.5 MB (53.11%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+          vector<vector<pair<int, int>>> adj(n + 1);
+                for (auto& time : times) {
+                    int u = time[0], v = time[1], w = time[2];
+                    adj[u].push_back({v, w});
+                }
+
+        // Initialize min-heap
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+        pq.push({0, k});
+
+        // Initialize distance array
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
+
+        // Process nodes
+        while (!pq.empty()) {
+            // Get the node with the smallest time
+            int time = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            // Traverse all neighbors of the current node
+            for (auto& it: adj[node]) {
+                    int nbr = it.first;
+                    int wt = it.second;
+                // If shorter path is found to neighbor
+                if (dist[nbr] > time + wt) {
+                    // Update distance
+                    dist[nbr] = time + wt;
+                    // Push updated time to the priority queue
+                    pq.push({dist[nbr], nbr});
+                }
+            }
+        }
+
+        // Get maximum time
+        int ans = *max_element(dist.begin() + 1, dist.end());
+        return ans == INT_MAX ? -1 : ans; 
+    }
+};
