8a31ff467becff2ec3d3c77acf94bdd75a86d1d7
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-14 17:16:20 +0530

    Time: 8 ms (23.16%), Space: 15.6 MB (22.04%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+
+    void createParent(TreeNode* root,
+                      unordered_map<TreeNode*,TreeNode*>& parent){
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+
+            TreeNode* node=q.front();
+            q.pop();
+
+            if(node->left){
+                parent[node->left]=node;
+                q.push(node->left);
+            }
+
+            if(node->right){
+                parent[node->right]=node;
+                q.push(node->right);
+            }
+        }
+    }
+
+    vector<int> distanceK(TreeNode* root,
+                          TreeNode* target,
+                          int k) {
+
+        unordered_map<TreeNode*,TreeNode*> parent;
+        createParent(root,parent);
+
+        unordered_map<TreeNode*,bool> visited;
+
+        queue<TreeNode*> q;
+        q.push(target);
+        visited[target]=true;
+
+        int cur_l=0;
+
+        while(!q.empty()){
+
+            int size=q.size();
+
+            if(cur_l==k)
+                break;
+
+            cur_l++;
+
+            for(int i=0;i<size;i++){
+
+                TreeNode* node=q.front();
+                q.pop();
+
+                if(node->left && !visited[node->left]){
+                    visited[node->left]=true;
+                    q.push(node->left);
+                }
+
+                if(node->right && !visited[node->right]){
+                    visited[node->right]=true;
+                    q.push(node->right);
+                }
+
+                if(parent[node] && !visited[parent[node]]){
+                    visited[parent[node]]=true;
+                    q.push(parent[node]);
+                }
+            }
+        }
+
+        vector<int> ans;
+
+        while(!q.empty()){
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+
+        return ans;
+    }
+};
