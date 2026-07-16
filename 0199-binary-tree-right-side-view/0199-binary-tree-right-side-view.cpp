b6f79f3d506b92fd86345b6e7c805748780550ee
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-17 01:54:39 +0530

    Time: 0 ms (100%), Space: 14.8 MB (85.71%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,19 +12,17 @@
 class Solution {
 public:
 
-    void recursion(TreeNode* root,int level, vector<int>& ans){
-        if(root==NULL) return ;
+    void recursion(TreeNode* root, int level , vector<int>& ans){
+        if(root == nullptr) return ;
 
-            if(level==ans.size())ans.push_back(root->val);
-            recursion(root->right,level+1,ans);
-            recursion(root->left,level+1,ans);
+        if(level == ans.size()) ans.push_back(root->val);
+        recursion(root->right,level+1,ans);
+        recursion(root->left,level+1,ans);
     }
 
-
     vector<int> rightSideView(TreeNode* root) {
-            vector<int>ans;
-            recursion(root,0,ans);
-            return ans;
-
+        vector<int>ans;
+        recursion(root,0,ans);
+        return ans;
     }
 };
