4b21c085e4a4e71c21d20e5b93d718654dffafa1
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-16 20:25:22 +0530

    Time: 99 ms (54.3%), Space: 92.2 MB (5.28%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -1,20 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int ans=0;
-    void solve(TreeNode* root,int maxNodeSeen){
-        if(!root) return;
-
-        if(root->val>=maxNodeSeen){
-            ans++;
-            maxNodeSeen=root->val;
-        }
-
-        solve(root->left,maxNodeSeen);
-        solve(root->right,maxNodeSeen);
-    }
     int goodNodes(TreeNode* root) {
-        if(!root) return 0;
-        solve(root,root->val);
-        return ans;
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,root->val});
+        int ans =0;
+
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            TreeNode* node = it.first;
+            int maxSofar = it.second;
+
+            if(node->val>=maxSofar){
+                maxSofar = node->val;
+                ans++;
+            }
+            if(node->left) q.push({node->left,maxSofar});
+            if(node->right) q.push({node->right,maxSofar});
+
+        } return ans;
     }
 };
