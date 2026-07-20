b3cd5d2058abe3d443b87285e128968c571a5347
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-21 03:00:00 +0530

    Time: 3 ms (80.69%), Space: 11.6 MB (82.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> st; // Changed to char to match the string
+        // Array to store the last seen index of each ASCII character
+        vector<int> hash(256, -1); 
+        
         int n = s.length();
-        int i = 0, j = 0; // Both pointers start at the beginning
+        int i = 0, j = 0;
         int maxi = 0;
 
         while (j < n) {
-            // If s[j] is NOT in the set, expand the window
-            if (st.find(s[j]) == st.end()) {
-                st.insert(s[j]);
-                maxi = max(maxi, j - i + 1);
-                j++;
-            } 
-            // If s[j] IS in the set, we have a duplicate. Shrink from the left.
-            else {
-                st.erase(s[i]);
-                i++;
+            // If we've seen this character before AND it's inside our current window
+            if (hash[s[j]] != -1 && hash[s[j]] >= i) {
+                // Jump the left pointer straight to the right of the duplicate
+                i = hash[s[j]] + 1; 
             }
-        } 
+            
+            // Record/Update the latest index of this character
+            hash[s[j]] = j;
+            maxi = max(maxi, j - i + 1);
+            j++;
+        }
         
         return maxi;
     }
