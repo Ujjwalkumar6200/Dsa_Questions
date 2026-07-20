09b5a7f2e59181ab142539383a452419a2db523c
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-21 02:47:01 +0530

    Time: 0 ms (100%), Space: 19.2 MB (68.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        for(auto it: prerequisites){
-            adj[it[1]].push_back(it[0]);
-        }
+        vector<vector<int>> adj(numCourses);
+        for(auto& it : prerequisites){
+            int u = it[0];
+            int v = it[1];
 
-        vector<int> indegree(numCourses);
+            adj[v].push_back(u);
+        }
+         vector<int> indegree(numCourses);
             for(int i =0;i<numCourses;i++){
                 for(auto it:adj[i]){
                     indegree[it]++;
                 }
             }
-        queue<int>q;
-        for(int i =0;i<numCourses;i++){
-            if(indegree[i]==0) q.push(i);
-        }
-        int cnt=0;
 
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            cnt++;
-
-            for(auto it: adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
+            queue<int>q;
+            for(int i =0;i<numCourses;i++){
+                if(indegree[i]==0){
+                    q.push(i);
+                }
             }
-        }
-        if(cnt==numCourses) return true;
-        return false;
+            int cnt =0;
 
+            while(!q.empty()){
+                auto node = q.front();
+                q.pop();
+
+                for(auto & it : adj[node]){
+                    indegree[it]--;
+                    if(indegree[it]==0) q.push(it);
+                } cnt++;
+            } if(cnt==numCourses) return true;
+            else return false;
     }
 };
