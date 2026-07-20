61901217c4a7fb13abb724fe522d768d390e9fd8
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-20 13:57:32 +0530

    Time: 29 ms (42.09%), Space: 17.6 MB (43.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,36 @@
 class Solution {
-private:
-      int n;
-      int m;
-public:
+ 
+ private:
 
-    void dfs(int row, int col,vector<vector<char>>& grid,int delrow[], int delcol[],vector<vector<int>>& vis){
-            if (row < 0 || col < 0 || col >= n || row >= m || grid[row][col] == '0') return;
-            for(int i =0;i<4;i++){
-                int nr = row + delrow[i];
-                int nc = col+delcol[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]=='1' && vis[nr][nc] == 1){
-                   vis[nr][nc]=0;
-                    dfs(nr,nc,grid,delrow,delcol,vis);
-                }
-            }
+ int n;
+ int m;
+
+ void dfs(int row,int col , const vector<vector<char>>& grid,vector<vector<int>>& visited){
+    int delrow[] = {-1,0,+1,0};
+    int delcol[] = {0,1,0,-1};
+
+    for(int i =0;i<4;i++){
+        int nr = row + delrow[i];
+        int nc = col + delcol[i];
+        if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc] == '1' && visited[nr][nc]==1){
+            visited[nr][nc] = 0;
+            dfs(nr,nc,grid,visited);
+        }
     }
-
+ }   
+public:
     int numIslands(vector<vector<char>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,1));
-
-        int delrow[] = {-1,0,+1,0};
-        int delcol[] = {0,1,0,-1};
+        vector<vector<int>> visited(m,vector<int>(n,1));
         int islands = 0;
+
         for(int i =0;i<m;i++){
             for(int j =0;j<n;j++){
-                if(grid[i][j]=='1'&& vis[i][j]==1){
-                    vis[i][j]=0;
+                if(grid[i][j]=='1' && visited[i][j]==1){
+                    visited[i][j]=0;
                     islands++;
-                    dfs(i,j,grid,delrow,delcol,vis);
+                    dfs(i,j,grid,visited);
                 }
             }
         } return islands;
