ea1bf3b46786361ace7059f1daf342e504ba72ef
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-22 19:18:26 +0530

    Time: 0 ms (100%), Space: 22.9 MB (84.22%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -15,16 +15,16 @@ public:
         if(root==NULL) return 0;
 
         int lh = height(root->left);
-        if(lh == -1) return -1;
+        if(lh==-1) return -1;
         int rh = height(root->right);
-        if(abs(lh-rh)>1) return -1;
-        if(rh==-1) return -1;
-        return 1+max(lh,rh);
+        if(rh == -1) return -1;
+        if(abs(lh-rh)>1) return  -1;
+
+        return 1 + max(lh,rh);
     }
 
-
     bool isBalanced(TreeNode* root) {
-        if(height(root)!=-1) return true;
-        return false;
+        if(height(root)==-1) return false;
+        else return true;
     }
 };
