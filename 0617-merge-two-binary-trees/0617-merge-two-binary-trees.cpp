a3a8596985ddc3cd8a8082b58b6eac4283476c11
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-07-30 13:45:09 +0530

    Time: 8 ms (5.79%), Space: 35.5 MB (5.5%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -1,24 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1==NULL) return root2;
-        if(root2==NULL) return root1;
 
-        TreeNode* node = new TreeNode(root1->val+root2->val);
+        if (root1 == NULL && root2 == NULL)
+            return NULL;
 
-        node->left  = mergeTrees(root1->left,root2->left);
-        node->right = mergeTrees(root1->right,root2->right);
+        if (root1 == NULL) {
+            TreeNode* node = new TreeNode(root2->val);
+            node->left = mergeTrees(NULL, root2->left);
+            node->right = mergeTrees(NULL, root2->right);
+            return node;
+        }
+
+        if (root2 == NULL) {
+            TreeNode* node = new TreeNode(root1->val);
+            node->left = mergeTrees(root1->left, NULL);
+            node->right = mergeTrees(root1->right, NULL);
+            return node;
+        }
+
+        TreeNode* node = new TreeNode(root1->val + root2->val);
+
+        node->left = mergeTrees(root1->left, root2->left);
+        node->right = mergeTrees(root1->right, root2->right);
 
         return node;
     }
