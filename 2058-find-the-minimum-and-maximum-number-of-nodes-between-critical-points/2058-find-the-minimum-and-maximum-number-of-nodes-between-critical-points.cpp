f95311bda1a9fb5238e236f5c569a646819b7ee2
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-08-31 12:38:39 +0530

    Time: 4 ms (57.39%), Space: 124.7 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,38 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        int cnt = 1;
-        vector<int> index;
         ListNode* prev = head;
-        ListNode* next = head->next;
-        while(next!=nullptr){
-            if(prev->val < head->val && next->val < head->val) index.push_back(cnt);
-            else if(prev->val > head->val && next->val > head->val) index.push_back(cnt);
-            prev = head;
-            head = head->next;
-            next = head->next;
-            cnt++;
-        }
-        if (index.size() < 2)
-            return {-1, -1};
+        ListNode* curr = head->next;
 
+        int pos = 1;
+        int first = -1;
+        int last = -1;
         int mn = INT_MAX;
 
-        for (int i = 1; i < index.size(); i++) {
-            mn = min(mn, index[i] - index[i - 1]);
+        while (curr->next != nullptr) {
+            ListNode* next = curr->next;
+
+            if ((curr->val > prev->val && curr->val > next->val) ||
+                (curr->val < prev->val && curr->val < next->val)) {
+
+                if (first == -1) {
+                    first = pos;
+                } else {
+                    mn = min(mn, pos - last);
+                }
+
+                last = pos;
+            }
+
+            prev = curr;
+            curr = next;
+            pos++;
         }
 
-        int mx = index.back() - index.front();
+        if (first == last)
+            return {-1, -1};
+
+        int mx = last - first;
 
         return {mn, mx};
     }
