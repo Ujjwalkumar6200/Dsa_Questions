81d2cdd70338bb21c947a332a9c1a6b40b4c17a9
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-09-02 16:34:37 +0530

    Time: 39 ms (71.61%), Space: 45.5 MB (55.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixCounts;
-        
-        // Base case: A prefix sum of 0 has occurred exactly 1 time (an empty subarray)
-        prefixCounts[0] = 1;
-        
-        int currentSum = 0;
-        int totalSubarrays = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            currentSum += nums[i];
-            
-            // If (currentSum - k) exists in the map, add its frequency to our total
-            int target = currentSum - k;
-            if (prefixCounts.count(target)) {
-                totalSubarrays += prefixCounts[target];
+        unordered_map<int,int> mpp;
+        mpp[0]=1;
+        int sum =0;
+        int longest =0;
+
+        for(int i =0;i<nums.size();i++){
+            sum+=nums[i];
+            int rem = sum-k;
+            if(mpp.find(rem)!=mpp.end()){
+                longest+=mpp[rem];
             }
-            
-            // Record the current prefix sum in the map for future indices to use
-            prefixCounts[currentSum]++;
-        }
-        
-        return totalSubarrays;
+            mpp[sum]++; 
+        } return longest;
     }
 };
