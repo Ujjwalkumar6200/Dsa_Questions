d10015a94e3bf5e650406a4b535412523ef4728e
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-09-25 13:30:36 +0530

    Time: 0 ms (100%), Space: 44 MB (79.83%) - LeetHub

--- 1330-reverse-subarray-to-maximize-array-value/1330-reverse-subarray-to-maximize-array-value.cpp
+++ 1330-reverse-subarray-to-maximize-array-value/1330-reverse-subarray-to-maximize-array-value.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int maxValueAfterReverse(vector<int>& A) {
+    int maxValueAfterReverse(vector<int>& nums) {
         int total = 0, res = 0;
         int mn = INT_MAX, mx = INT_MIN;
-        int n = A.size();
+        int n = nums.size();
 
         for (int i = 0; i < n - 1; i++) {
-            int a = A[i], b = A[i + 1];
+            int a = nums[i], b = nums[i + 1];
 
             total += abs(a - b);
 
-            res = max(res, abs(A[0] - b) - abs(a - b));
-            res = max(res, abs(A[n - 1] - a) - abs(a - b));
+            res = max(res, abs(nums[0] - b) - abs(a - b));
+            res = max(res, abs(nums[n - 1] - a) - abs(a - b));
 
             mn = min(mn, max(a, b));
             mx = max(mx, min(a, b));
