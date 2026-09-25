5eb62ac7e1234bde01c84f4a08d1a00ab048ce5c
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-09-25 12:06:06 +0530

    Time: 0 ms (100%), Space: 44 MB (55.46%) - LeetHub

--- 1330-reverse-subarray-to-maximize-array-value/1330-reverse-subarray-to-maximize-array-value.cpp
+++ 1330-reverse-subarray-to-maximize-array-value/1330-reverse-subarray-to-maximize-array-value.cpp
@@ -1,48 +1,22 @@
 class Solution {
 public:
-    int maxValueAfterReverse(vector<int>& nums) {
-        int n = nums.size();
-
-        int initial = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            initial += abs(nums[i] - nums[i + 1]);
-        }
-
-        int gain = 0;
-
-        // Reverse subarray [0 ... j]
-        for (int j = 1; j < n; j++) {
-            gain = max(
-                gain,
-                abs(nums[0] - nums[j]) -
-                abs(nums[j - 1] - nums[j])
-            );
-        }
-
-        // Reverse subarray [i ... n-1]
-        for (int i = 0; i < n - 1; i++) {
-            gain = max(
-                gain,
-                abs(nums[n - 1] - nums[i]) -
-                abs(nums[i] - nums[i + 1])
-            );
-        }
-
-        // Reverse a middle subarray [i ... j]
-        int mx = INT_MIN;
-        int mn = INT_MAX;
+    int maxValueAfterReverse(vector<int>& A) {
+        int total = 0, res = 0;
+        int mn = INT_MAX, mx = INT_MIN;
+        int n = A.size();
 
         for (int i = 0; i < n - 1; i++) {
-            int a = nums[i];
-            int b = nums[i + 1];
+            int a = A[i], b = A[i + 1];
 
-            mx = max(mx, min(a, b));
+            total += abs(a - b);
+
+            res = max(res, abs(A[0] - b) - abs(a - b));
+            res = max(res, abs(A[n - 1] - a) - abs(a - b));
+
             mn = min(mn, max(a, b));
-
-            gain = max(gain, 2 * (mx - mn));
+            mx = max(mx, min(a, b));
         }
 
-        return initial + gain;
+        return total + max(res, 2 * (mx - mn));
     }
 };
