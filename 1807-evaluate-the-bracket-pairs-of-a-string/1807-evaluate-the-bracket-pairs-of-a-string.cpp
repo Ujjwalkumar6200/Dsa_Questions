b746f03216f074772f701164e552bbf66e159a26
Ujjwal kumar <Ujjwalkumargrd3@gmail.com>
2026-09-26 16:46:35 +0530

    Time: 79 ms (60.44%), Space: 122.6 MB (76.01%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -3,25 +3,31 @@ public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         string ans = "";
         string now = "";
-        unordered_map<string,string> mpp;
-        for(int i =0;i<knowledge.size();i++){
+
+        unordered_map<string, string> mpp;
+
+        for(int i = 0; i < knowledge.size(); i++) {
             mpp[knowledge[i][0]] = knowledge[i][1];
         }
 
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(') {
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == '(') {
                 i++;
                 now = "";
 
-                while (s[i] != ')') {
+                while(s[i] != ')') {
                     now += s[i];
                     i++;
                 }
-                if(mpp.find(now)!=mpp.end()) ans+=mpp[now];
-                else ans+='?';
 
-            } else {
+                auto it = mpp.find(now);
+
+                if(it != mpp.end())
+                    ans += it->second;
+                else
+                    ans += '?';
+            }
+            else {
                 ans += s[i];
             }
         }
